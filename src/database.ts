79e0c23f6748467601f78e60b2b89d1dfb1fcf59
Jeffersson Semin <69760@dkmcdesadm14.local>
2025-04-10 10:42:48 -0300

    add first migration and setup config

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,14 @@
-import knex from 'knex'
+import knex, { Knex } from 'knex'
 
-export const conn = knex({
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/app.db',
   },
-})
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+export const conn = knex(config)
